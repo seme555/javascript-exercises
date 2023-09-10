b7b8ce2e9829f42f3070cfd8090460086158f086
seme <126499779+seme555@users.noreply.github.com>
2023-09-10 17:44:56 +0300

    ex2 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+ 
+    let str=""
+    if(string===''){
 
+    }
+    if( num<0){
+    return "ERROR"
+    }
+    
+for(let i=0; i<num;i++){
+   
+ 
+str=str +string 
+}
+return str
 };
 
 // Do not edit below this line
