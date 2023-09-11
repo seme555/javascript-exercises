9a902df8e575077d318f2c147ea1868a68321eae
seme <126499779+seme555@users.noreply.github.com>
2023-09-11 13:59:01 +0300

    ex4 final

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+   let array=args[0]
+   let newArray=[]
+  function remover(item){
+      if(!args.includes(item)){
+        
+        newArray.push(item)
+      }
 
+  }
+
+   array.forEach(remover)
+   return newArray;
 };
 
 // Do not edit below this line
