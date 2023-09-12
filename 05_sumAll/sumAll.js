4cf5790d88fdd3798d6ab50dd794d4b4ebab5c0d
seme <126499779+seme555@users.noreply.github.com>
2023-09-12 19:11:04 +0300

    ex5 easyyyyy

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function( a,b) {
+    let sum=0;
+if(a>0 && b>0 && typeof a==="number" && typeof b==="number"){
 
+ 
+if(a<b){
+ for(let i=a; i<=b; i++){
+
+ sum+=i;
+ }
+
+}
+
+if(a>b){
+    for(let i=b; i<=a; i++){
+   
+    sum+=i;
+    }
+   
+   }
+
+
+}
+else{
+
+    return "ERROR"
+}
+return sum;
 };
 
 // Do not edit below this line
