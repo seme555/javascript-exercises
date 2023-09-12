4309e73c64c970b08e0dd064a17d1cbdf548364f
seme <126499779+seme555@users.noreply.github.com>
2023-09-12 20:44:43 +0300

    ex7 easy!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+
+ let c_rounded;
+ c_rounded= Math.round(5*(f-32)*10/9)/10
+ 
+return c_rounded
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+
+  let f_rounded;
+ f_rounded= Math.round((9*c/5+32)*10)/10
+ 
+return f_rounded
+
 };
 
 // Do not edit below this line
