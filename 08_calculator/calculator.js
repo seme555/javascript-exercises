68ff1d8bbfc7164147b2a068aa548b76df25c7a3
seme <126499779+seme555@users.noreply.github.com>
2023-09-12 21:41:27 +0300

    ex8 minor

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,58 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum=0
+  if(array.length===0){
+   sum=sum
+  }
+	else{
+   for(let i=0; i<array.length; i++){
+
+    sum=sum + array[i]
+   }
+
+  }
+  
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let mult=1
+   for(let i=0;i<array.length;i++ ){
+    mult=mult*array[i]
 
+   }
+
+return mult
 };
 
-const power = function() {
-	
+const power = function(n,p) {
+  let pow=1
+	for(let i=0;i<p; i++){
+   pow=pow*n
+
+  }
+  return pow
 };
 
-const factorial = function() {
-	
+const factorial = function(f) {
+  let fact=1
+  if(f===0 || f===1){
+    fact===1
+  }
+	else{
+    for(let i=f; i>1;i--){
+
+      fact=fact*i
+    }
+  }
+  return fact
 };
 
 // Do not edit below this line
