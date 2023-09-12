fec59e12a2fa14af101a838953540e239c374f08
seme <126499779+seme555@users.noreply.github.com>
2023-09-12 19:55:43 +0300

    ex6 very easy

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+if(year%4===0){
+    if( year%100===0  ){
+     if(year%400===0){
+        year=true
+     }
+     else{
+        year=false
+     }
+    }
 
+else{
+
+    year=true
+}
+}
+
+else{
+    year=false
+}
+
+return year
 };
 
 // Do not edit below this line
